5dd89e16770e3a350aed3b4fcea2c1f7d5094030
Abu Bakar Halid <bangabugrafis@gmail.com>
2022-11-21 17:15:19 +0800

    update mouse hover effect

--- app.js
+++ app.js
@@ -10,8 +10,29 @@ new Vue({
             .then(json => {
                 this.bracket = json;
         });
+        this.isteam()
     },
     methods: {
-      //
+      isteam() {
+        setTimeout(function(){
+            let team = document.querySelectorAll("div.teams")
+            for (let i = 0; i < team.length; i++) {
+                team[i].onmouseenter = function() {
+                    let teams = document.querySelectorAll('.'+team[i].dataset.teamId)
+                    for (let j = 0; j < teams.length; j++) {
+                        teams[j].classList.toggle("active");
+                    }
+                };
+            }
+            for (let i = 0; i < team.length; i++) {
+                team[i].onmouseleave = function() {
+                    let teams = document.querySelectorAll('.'+team[i].dataset.teamId)
+                    for (let j = 0; j < teams.length; j++) {
+                        teams[j].classList.toggle("active");
+                    }
+                };
+            }
+        }, 500)
+      }
     }
   });
